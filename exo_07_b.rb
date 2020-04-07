22ccb68c0805ede56fb081a2cce1913fd30c925b
David Bruggisser <dav.bruggisser@gmail.com>
2020-04-07 22:58:41 +0200

    Finished exercise 07_b

---
+++ exo_07_b.rb
@@ -0,0 +1,8 @@
+puts "Bonjour, c'est quoi ton blase ?"
+print "> "
+user_name = gets.chomp
+puts user_name
+
+=begin
+When we use print, the next item printed is going to be on the same line and not with a line break
+=end
