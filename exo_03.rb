a312dbeb8f54c0fa1ad3f19ffb5f5f88b4497d37
David Bruggisser <dav.bruggisser@gmail.com>
2020-04-07 21:28:45 +0200

    Improved exercise 3

--- exo_03.rb
+++ exo_03.rb
@@ -1,3 +1,6 @@
 puts "Bonjour, monde !"
 #puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
-#The hashtag sign can be used for single line comments
+
+=begin
+The # character is used for single line comments
+=end
