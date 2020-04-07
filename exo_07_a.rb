ebd2ed3b0de116b7fdb3384c74571356744d6db6
David Bruggisser <dav.bruggisser@gmail.com>
2020-04-07 22:55:44 +0200

    Finished exercise 07_a

---
+++ exo_07_a.rb
@@ -0,0 +1,7 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
+
+=begin
+gets means "get string" so it gets a string input from the user. I'm not sure I really understand chomp though.
+=end
