d684ceb572efa1ec3f5012c52b09799069119f74
David Bruggisser <dav.bruggisser@gmail.com>
2020-04-07 23:14:45 +0200

    Finished exercise 16

--- exo_16.rb
+++ exo_16.rb
@@ -1,8 +1,6 @@
 puts "Quel est ton âge ?"
 age = gets.chomp.to_i
-birth_date = (2019 - age)
 i = 0
-years = 2019 - birth_date
-years.times do
-  puts "Il y a #{years -=1} ans, tu avais #{ i +=1} ans."
+age.times do
+  puts "Il y a #{age -=1} ans, tu avais #{ i +=1} ans."
 end
