39f383b33c66f1347c78e99da1b14fec93b27263
David Bruggisser <dav.bruggisser@gmail.com>
2020-04-07 12:04:34 +0200

    Made changes to exercises 3 and 4

---
+++ exo_03.rb
@@ -0,0 +1,3 @@
+puts "Bonjour, monde !"
+#puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
+#The hashtag sign can be used for single line comments
---
+++ exo_04.rb
@@ -0,0 +1,2 @@
+puts "Salut, ça farte ?
+#I forgot to put the " symbol after the string so I get an error when I try to print the string.
