82f54607f51c0133d5816868168d074b0cf44ba1
David Bruggisser <dav.bruggisser@gmail.com>
2020-04-07 12:04:54 +0200

    Finished exercise 5

---
+++ exo_05.rb
@@ -0,0 +1,22 @@
+puts "On va compter le nombre d'heures de travail à THP" #prints a string
+puts "Travail : #{10 * 5 * 11}" #prints Travail : 550
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" #prints En minutes ça fait : 33000 #{} prints the math result inside a string
+
+puts "Et en secondes ?" #prints a string
+
+puts 10 * 5 * 11 * 60 * 60 #prints 1980000
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" #prints a string
+
+puts 3 + 2 < 5 - 7 #prints false
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" #prints Ça fait combien 3 + 2 ? 5
+puts "Ça fait combien 5 - 7 ? #{5 - 7}" #prints Ça fait combien 5 - 7 ? -2
+
+puts "Ok, c'est faux alors !" #prints a string
+
+puts "C'est drôle ça, faisons-en plus :" #prints a string
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" #prints Est-ce que 5 est plus grand que -2 ? true
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" #prints Est-ce que 5 est supérieur ou égal à -2 ? true
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" #prints Est-ce que 5 est inférieur ou égal à -2 ? false
