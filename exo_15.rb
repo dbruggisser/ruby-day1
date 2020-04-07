d8d5639d97f6f000343e60d6a5d5b1f25bf7f118
David Bruggisser <dav.bruggisser@gmail.com>
2020-04-07 23:12:53 +0200

    Fixed an issue with the year and removed a variable

--- exo_15.rb
+++ exo_15.rb
@@ -1,6 +1,6 @@
 puts "Quelle est ton année de naissance ?"
 birth_date = gets.chomp.to_i
-age = 0
-(2019 - birth_date).times do
-  puts "#{birth_date += 1} Age :#{age +=1}"
+i = 0
+(2020 - birth_date).times do
+  puts "#{birth_date += 1} Age :#{i +=1}"
 end
