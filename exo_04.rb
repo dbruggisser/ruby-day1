75a866ea66aea2f10a2778216e0a780a6ca3679f
David Bruggisser <dav.bruggisser@gmail.com>
2020-04-07 22:33:13 +0200

    Improved exercise 4

--- exo_04.rb
+++ exo_04.rb
@@ -1,2 +1,5 @@
 puts "Salut, ça farte ?
-#I forgot to put the " symbol after the string so I get an error when I try to print the string.
+
+=begin
+I forgot to put the " symbol after the string so I get an error when I try to print the string.
+=end
